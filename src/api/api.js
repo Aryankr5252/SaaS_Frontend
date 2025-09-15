4b26796f522d79d04bbcc8b486fab6bafe94b55c
Aryankr5252 <aryanak5252@gmail.com>
2025-09-15 21:23:29 +0530

    api changes

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE + '/api',
+  baseURL: import.meta.env.VITE_API_BASE + '/api' || "http://localhost:5001/api",
   withCredentials: true,
 });
 
